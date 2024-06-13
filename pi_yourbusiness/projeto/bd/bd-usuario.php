44af294edb86636e000779d5c63cebdbb334c0fb
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-06-12 21:28:38 -0300

    Changes in bd-usuarios

--- pi_yourbusiness/projeto/bd/bd-usuario.php
+++ pi_yourbusiness/projeto/bd/bd-usuario.php
@@ -1,23 +1,112 @@
 <?php
-global $conn;
-$linha= 0;
-//falta isset no $linha
-require '../utils/conexao.php';
+require "../utils/conexao.php";
 
 // Verifica se a conexão foi bem-sucedida
-if (!isset($conn) || $conn->connect_error) {
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$acao = isset($_POST['acao']) ? $_POST['acao'] : null;
+$id_usuario = isset($_POST['id_usuario']) ? $_POST['id_usuario'] : null;
+$nome = isset($_POST['nome']) ? $_POST['nome'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+$telefone = isset($_POST['telefone']) ? $_POST['telefone'] : null;
+$data_nascimento = isset($_POST['data_nascimento']) ? $_POST['data_nascimento'] : null;
+$cpf = isset($_POST['cpf']) ? $_POST['cpf'] : null;
+$nivel_acesso = isset($_POST['nivel_acesso']) ? $_POST['nivel_acesso'] : null;
+$cep = isset($_POST['cep']) ? $_POST['cep'] : null;
+$endereco = isset($_POST['endereco']) ? $_POST['endereco'] : null;
+$numero = isset($_POST['numero']) ? $_POST['numero'] : null;
+$complemento = isset($_POST['complemento']) ? $_POST['complemento'] : null;
+$cidade = isset($_POST['cidade']) ? $_POST['cidade'] : null;
+$estado = isset($_POST['estado']) ? $_POST['estado'] : null;
+$senha = isset($_POST['senha']) ? $_POST['senha'] : null;
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($acao == "INCLUIR") {
+        $sql = "INSERT INTO usuarios (nome, email, telefone, data_nascimento, cpf, nivel_acesso, cep, endereco, numero, complemento, cidade, estado, senha) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-// Prepara a consulta SQL
+        $stmt = $conn->prepare($sql);
+        if ($stmt === false) {
+            die("Prepare failed: " . $conn->error);
+        }
+
+        $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
+        $stmt->bind_param("sssssssssssss", $nome, $email, $telefone, $data_nascimento, $cpf, $nivel_acesso, $cep, $endereco, $numero, $complemento, $cidade, $estado, $hashed_password);
+
+        try {
+            if ($stmt->execute()) {
+                $idCadastro = $conn->insert_id;
+                echo "ID do Cadastro: " . $idCadastro;
+            } else {
+                echo "Erro na execução: " . $stmt->error;
+            }
+        } catch (Exception $e) {
+            echo "Erro ao cadastrar: " . $e->getMessage();
+            echo '<script>history.back();</script>';
+        }
+
+        $stmt->close();
+    } elseif ($acao == "ALTERAR") {
+        if ($senha) {
+            $sql = "UPDATE usuarios SET nome = ?, email = ?, telefone = ?, data_nascimento = ?, cpf = ?, nivel_acesso = ?, cep = ?, endereco = ?, numero = ?, complemento = ?, cidade = ?, estado = ?, senha = ? WHERE id_usuario = ?";
+            $stmt = $conn->prepare($sql);
+            $hashed_password = password_hash($senha, PASSWORD_DEFAULT);
+            $stmt->bind_param("sssssssssssssi", $nome, $email, $telefone, $data_nascimento, $cpf, $nivel_acesso, $cep, $endereco, $numero, $complemento, $cidade, $estado, $hashed_password, $id_usuario);
+        } else {
+            $sql = "UPDATE usuarios SET nome = ?, email = ?, telefone = ?, data_nascimento = ?, cpf = ?, nivel_acesso = ?, cep = ?, endereco = ?, numero = ?, complemento = ?, cidade = ?, estado = ? WHERE id_usuario = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ssssssssssssi", $nome, $email, $telefone, $data_nascimento, $cpf, $nivel_acesso, $cep, $endereco, $numero, $complemento, $cidade, $estado, $id_usuario);
+        }
+
+        if ($stmt === false) {
+            die("Prepare failed: " . $conn->error);
+        }
+
+        try {
+            if ($stmt->execute()) {
+                echo "Usuário atualizado com sucesso.";
+            } else {
+                echo "Erro na execução: " . $stmt->error;
+            }
+        } catch (Exception $e) {
+            echo "Erro ao atualizar: " . $e->getMessage();
+            echo '<script>history.back();</script>';
+        }
+
+        $stmt->close();
+    } elseif ($acao == "DELETAR") {
+        $sql = "DELETE FROM usuarios WHERE id_usuario = ?";
+        $stmt = $conn->prepare($sql);
+        if ($stmt === false) {
+            die("Prepare failed: " . $conn->error);
+        }
+
+        $stmt->bind_param("i", $id_usuario);
+
+        try {
+            if ($stmt->execute()) {
+                echo "Usuário deletado com sucesso.";
+            } else {
+                echo "Erro na execução: " . $stmt->error;
+            }
+        } catch (Exception $e) {
+            echo "Erro ao deletar: " . $e->getMessage();
+            echo '<script>history.back();</script>';
+        }
+
+        $stmt->close();
+    } else {
+        header("Location: /pi_yourbusiness/");
+        exit;
+    }
+
+    $conn->close();
+}
+
 $sql = "SELECT id, nome, razao, tamanho, fundacao, tributacao FROM produtos;";
-
-// Executa a consulta SQL
 $resultado = $conn->query($sql);
-
-// Verifica se a consulta foi bem-sucedida
 if (!$resultado) {
     die("Query failed: " . $conn->error);
 }
@@ -35,97 +124,24 @@ if (!$resultado) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista De Produtos</title>
     <style>
-        body {
-            background-color: #343a40;
-            color: #ffffff;
-            margin-top: 20px;
-        }
-
-        .container {
-            background-color: #343a40;
-            border-radius: 10px;
-            padding: 20px;
-            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
-        }
-
-        .btn-primary {
-            background-color: #007bff;
-            border-color: #007bff;
-        }
-
-        .btn-primary:hover {
-            background-color: #0056b3;
-            border-color: #0056b3;
-        }
-
-        .btn-danger {
-            background-color: #dc3545;
-            border-color: #dc3545;
-        }
-
-        .btn-danger:hover {
-            background-color: #c82333;
-            border-color: #bd2130;
-        }
-
-        .btn-secondary {
-            background-color: #6c757d;
-            border-color: #6c757d;
-        }
-
-        .btn-secondary:hover {
-            background-color: #5a6268;
-            border-color: #545b62;
-        }
-
-        .form-control {
-            background-color: #495057;
-            color: #ffffff;
-            border-color: #495057;
-        }
-
-        .form-control:focus {
-            background-color: #495057;
-            color: #ffffff;
-            border-color: #495057;
-            box-shadow: none;
-        }
-
-        .border {
-            border-color: #ffffff !important;
-        }
-
-        .thead-dark th {
-            background-color: #343a40;
-            color: #ffffff;
-            border-color: #ffffff;
-        }
-
-        .rounded {
-            border-radius: 10px !important;
-        }
-
-        .table-striped tbody tr:nth-of-type(odd) {
-            background-color: #495057;
-        }
-
-        .badge {
-            border-radius: 10px;
-            font-size: 12px;
-            padding: 6px 10px;
-        }
-
-        .badge-primary {
-            background-color: #007bff;
-        }
-
-        .badge-secondary {
-            background-color: #6c757d;
-        }
-
-        .text-white {
-            color: #ffffff !important;
-        }
+        body { background-color: #343a40; color: #ffffff; margin-top: 20px; }
+        .container { background-color: #343a40; border-radius: 10px; padding: 20px; box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75); }
+        .btn-primary { background-color: #007bff; border-color: #007bff; }
+        .btn-primary:hover { background-color: #0056b3; border-color: #0056b3; }
+        .btn-danger { background-color: #dc3545; border-color: #dc3545; }
+        .btn-danger:hover { background-color: #c82333; border-color: #bd2130; }
+        .btn-secondary { background-color: #6c757d; border-color: #6c757d; }
+        .btn-secondary:hover { background-color: #5a6268; border-color: #545b62; }
+        .form-control { background-color: #495057; color: #ffffff; border-color: #495057; }
+        .form-control:focus { background-color: #495057; color: #ffffff; border-color: #495057; box-shadow: none; }
+        .border { border-color: #ffffff !important; }
+        .thead-dark th { background-color: #343a40; color: #ffffff; border-color: #ffffff; }
+        .rounded { border-radius: 10px !important; }
+        .table-striped tbody tr:nth-of-type(odd) { background-color: #495057; }
+        .badge { border-radius: 10px; font-size: 12px; padding: 6px 10px; }
+        .badge-primary { background-color: #007bff; }
+        .badge-secondary { background-color: #6c757d; }
+        .text-white { color: #ffffff !important; }
     </style>
 </head>
 <body>
@@ -135,110 +151,33 @@ if (!$resultado) {
                 <legend class="w-auto p-1 border rounded">Lista De Produtos</legend>
                 <div class="row">
                     <div class="col-sm-2">
-                        <a href="../../projeto/dashboard.php" class="btn btn-primary">Voltar</a>
-                    </div>
-                    <div class="col-sm-4">
-                        <label for="txt_nome">Nome:</label>
-                        <input class="form-control" id="txt_nome" name="txt_nome" type="text" placeholder="Pesquise o Nome:">
-                    </div>
-                    <div class="col-sm-2">
-                        <label for="txt_data_inicial">Data Inicial:</label>
-                        <input class="form-control" id="txt_data_inicial" name="txt_data_inicial" type="date">
-                    </div>
-                    <div class="col-sm-2">
-                        <label for="txt_data_final">Data Final:</label>
-                        <input class="form-control" id="txt_data_final" name="txt_data_final" type="date">
-                    </div>
-                    <div class="col-sm-2">
-                        <button class="btn btn-primary align-self-center">Buscar</button>
-                    </div>
-                </div>
-            </fieldset>
+                        <a href="../../projeto/dashboard.php
 
-            <table class="table rounded table-striped mt-3">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>Ação</th>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Razão Social</th>
-                        <th>Tamanho</th>
-                        <th>Fundação</th>
-                        <th>Tributação</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    while ($linha = $resultado->fetch_assoc()) { // arrumar loop, ctz q e aq
-                        ?>
-                        <tr scope="row">
-                            <td>
-                                <a href="#excluir" class="btn btn-danger"> 
-                                    <!-- arrumar o botão excluir dnv -->
-                                    <i class="fas fa-trash"></i> 
-                                </a>
-                                <a href="cadastro_produto.php?id=<?= $linha['id'] ?>" class="btn btn-secondary">
-                                    <i class="fas fa-pen"></i>
-                                </a>
-                            </td>
-                            <td><?= $linha['id'] ?></td>
-                            <td><?= $linha['nome'] ?></td>
-                            <td><?= $linha['razao'] ?></td>
-                            <td><?= $linha['tamanho'] ?></td>
-                            <td><?= $linha['fundacao'] ?></td>
-                            <td><?= $linha['tributacao'] ?></td>
-                        </tr>
-                        <?php
+    <!--  
+    try {
+    if ($stmt)}
+    $stmt -> bind_param(
+    )
+    id do usuario no final dos dois bind param
+    e as variavies do bd-usuario sao, nessa ordem:
+    nome
+    email
+    celular
+    datanascimento
+    cpf
+    nivel acesso
+    cep
+    endereco
+    numero
+    complemento
+    cidade
+    estado
+    confirmasenha
+    idusuario
 
-                        // se a senha veio preenchida
-                        if($senha != "") {
-                            $sql ="UPDATE usuarios SET
-                            nome = ?,
-                            nivel_acesso = ?,
-                            cep = ?,
-                            endereco = ?,
-                            numero = ?,
-                            complemento = ?,
-                            cidade = ?,
-                            estado = ?,
-                            senha = ?,
-                            email =?,
-                            telefone=?,
-                            data_nascimento = ?,
-                            cpf= ?
-                        
-                            WHERE id_usuario = ?";
-                        }// com senha
+    acao
 
-                        else{
 
-                            $sql ="UPDATE usuarios SET
-                            nome = ?,
-                            nivel_acesso = ?,
-                            cep = ?,
-                            endereco = ?,
-                            numero = ?,
-                            complemento = ?,
-                            cidade = ?,
-                            estado = ?,
-                            email =?,
-                            telefone=?,
-                            data_nascimento =?,
-                            cpf= ?
-                        
-                            WHERE id_usuario = ?";
-                        
-                        }// sem senha        
-                    ?>
-                </tbody>
-            </table>
-        </form>
-    </div>
-    <footer class="footer bg-white border-top border-2 border-dark mt-5"> 
-        <!-- melhorar design do footer --> 
-        <div class="container text-center py-3">
-            Projeto desenvolvido por: Nikolas Arruda
-        </div>
-    </footer>
-</body>
-</html>
+    bind param com o sssis la n sei fazer
+
+    -->
