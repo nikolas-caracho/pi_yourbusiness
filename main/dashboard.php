292ed2fd9c610298e772fdf818a95202d9da071c
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-06-26 11:40:52 -0300

    CSS

--- main/dashboard.php
+++ main/dashboard.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Yourbusiness</title>
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <style>
+  <style>
         body {
             background-color: #343a40;
             color: #ffffff;
